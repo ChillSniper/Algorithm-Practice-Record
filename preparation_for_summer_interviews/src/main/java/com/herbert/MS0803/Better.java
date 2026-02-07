0d1866acf4af7ab168e4dfa25b6f6405475dcd4e
Lu_XueCheng <luxuecheng@tuyoogame.com>
2026-02-07 23:15:45 +0800

    exercise on 0207

--- preparation_for_summer_interviews/src/main/java/com/herbert/MS0803/Better.java
+++ preparation_for_summer_interviews/src/main/java/com/herbert/MS0803/Better.java
@@ -1,5 +1,21 @@
 package com.herbert.MS0803;
 
 public class Better {
-    
+    public int findMagicIndex(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i ++) {
+            nums[i] -= i;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            int val = nums[mid];
+            if (val > 0) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return r;
+    }
 }
