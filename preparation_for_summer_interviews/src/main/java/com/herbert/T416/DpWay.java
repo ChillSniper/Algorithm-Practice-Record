40f18b09f3254f51a8361bba5bb913794aaf1d82
Lu_XueCheng <luxuecheng@tuyoogame.com>
2026-02-20 22:25:28 +0800

    exercise on mac

--- preparation_for_summer_interviews/src/main/java/com/herbert/T416/DpWay.java
+++ preparation_for_summer_interviews/src/main/java/com/herbert/T416/DpWay.java
@@ -1,5 +1,23 @@
 package com.herbert.T416;
 
 public class DpWay {
-    
+    public boolean canPartition(int[] nums) {
+        int all = 0;
+        for (int x : nums)
+            all += x;
+        if (all % 2 == 1)
+            return false;
+        all /= 2;
+        int n = nums.length;
+        boolean f[][] = new boolean[n + 1][all + 1];
+        // pay attention to the init step
+        f[0][0] = true;
+        for (int i = 0; i < n;i ++) {
+            for (int j = 0;j <= all;j ++) {
+                int x = nums[i];
+                f[i + 1][j] = j >= x && f[i][j - x] || f[i][j];
+            }
+        }
+        return f[n][all];
+    }
 }
